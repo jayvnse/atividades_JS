3f1109cb82c2e1a00a547576b9e710c60b9d5cea
jacksonvne <jacksonsg@gmail.com>
2022-02-05 13:52:40 -0300

    subindo alteracoes que ficaram para tras

---
+++ quarta02/assets/index.js
@@ -0,0 +1,12 @@
+function mostrar(){
+    var mensagem = window.document.getElementsById('texto')
+    var foto = window.document.getElementById('minha-imagem')
+
+    var opcao = 1
+
+    if(opcao==1){
+        foto.src = './assets/pexels-tembela-bohle-3625609.jpg'
+    } else{
+        
+    }
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+window.alert('SEJA BEM VINDO A ESSA PÁGINA!')
+window.confirm('Você está aprendendo JS?')
+window.prompt('Digite seu nome:')
+
